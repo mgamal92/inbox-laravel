61dd4931d8ac9c3b694f8d081d80e3e48779b2a2
Mohamed Gamal <mg.dev1992@gmail.com>
2024-02-17 21:12:38 +0200

    add migration file

---
+++ database/migrations/2024_02_17_000001_create_inbox_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('inbox', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
+            $table->text('subject')->nullable();
+            $table->text('body');
+            $table->boolean('is_read')->default(false);
+            $table->boolean('is_starred')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('sender_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('receiver_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('laravel_inbox_table');
+    }
+};
